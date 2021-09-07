a1ece1a6c8a05092acf4b83ba9f5b7490983c85a
Rakibul71 <rakibulislam202019@gmail.com>
2021-09-07 14:34:54 +0600

    hero commit

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="architecture">
+        <div className="architecture-left">
+          <h2>ARCHITECTURE</h2>
+          <button>service</button>
+          <h4>
+            architecture is not any subject <br /> to read. (architecture) it's
+            a lifestyle to lead.
+          </h4>
+        </div>
+        <div className="architecture-right">
+          every society has an infrastructure by which the society leads to the
+          development improving it. in every parts of our lives architecture
+          remains to build our tiny dreams. you see a dream and we architects
+          work from visualization to make your dream come true. it can be said
+          that architecture is such art which leads our lives in a beautiful
+          way.
+        </div>
+      </div>
+      <div className="blog">
+        <div className="blog-left">
+          <h2>BLOG</h2>
+          <button>recent</button>
+          <h4>
+            the place to share our ideas. <br /> through these blogs our
+            interaction <br />
+            will be more useful
+          </h4>
+        </div>
+        <div className="blog-right">
+          we human learn from every aspects of their lives. through several
+          mediums we share those to one another. through our blogs we will share
+          our learnings,ideas, relatable things to gather some useful
+          knowledges. so that our ideas can be widen in a large quantity. our
+          sharing ideas will work as mass communicative knowledge.
+        </div>
+      </div>
+      <div className="course">
+        <div className="course-left">
+          <h2>COURSE</h2>
+          <button>recent</button>
+          <h4>
+            the way to enlighten you with our <br /> knowledge. our courses are
+            for those <br />
+            who are enthusiastic to get prepared <br /> in our fields.
+          </h4>
+        </div>
+        <div className="course-right">
+          In modern architectural relatable practice we need several types of
+          knowledges if we want to get involved with such fields. so for those
+          who are keen to gather modern architectural knowledges and practical
+          learnings we have special coures for up to date universal practical
+          knowledges for professional fields. besides we want to make you
+          involved with such relatable knowledge through these courses which can
+          be supportive for those.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import Slider from './Slider/Slider';
-import Clients from './Clients/Clients'
-import Team from './Team/Team' 
-import Aim from './Aim/Aim';
-import Footer from './Footer/Footer';
+import React from "react";
+import Slider from "./Slider/Slider";
+import Clients from "./Clients/Clients";
+import Team from "./Team/Team";
+import Aim from "./Aim/Aim";
+import Footer from "./Footer/Footer";
+import Hero from "../Hero/Hero";
 
-const Home=()=>{
-    return(
-        <>
-            <Slider />
-            <Team />
-            <Clients />
-            <Aim />
-            <Footer />
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <>
+      <Slider />
+      {/* <Team /> */}
+      {/* <Clients /> */}
+      {/* <Aim /> */}
+      <Hero></Hero>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
